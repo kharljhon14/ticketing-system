fbd0bcb420081c85366b77bb0e505e141864e05b
Kharl <enriquezkharl14@gmail.com>
2024-10-09 22:30:16 +0800

    [Feat] Added create new ticket route

---
+++ src/app/api/ticket/route.ts
@@ -0,0 +1,23 @@
+import { ticketSchema } from "@/schemas/ticket";
+import { NextResponse, type NextRequest } from "next/server";
+import prisma from "../../../../prisma/db";
+
+export async function POST(request: NextRequest) {
+	const body = await request.json();
+
+	// Validate the body object
+	const validation = ticketSchema.safeParse(body);
+
+	// Return error message
+	if (!validation.success) {
+		return NextResponse.json(validation.error.format(), { status: 400 });
+	}
+
+	// Create new ticket
+	const newTicket = await prisma.ticket.create({
+		data: { ...body },
+	});
+
+	// Return new ticket
+	return NextResponse.json(newTicket, { status: 201 });
+}
