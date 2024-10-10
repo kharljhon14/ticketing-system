f165d4c3162e9e0e878b86b8816d6e444294aa0a
Kharl <enriquezkharl14@gmail.com>
2024-10-10 21:45:57 +0800

    [Feat] Linking ticket to ticket page

--- src/features/tickets/ticket-table.tsx
+++ src/features/tickets/ticket-table.tsx
@@ -1,4 +1,5 @@
 import type { Ticket } from "@prisma/client";
+import Link from "next/link";
 
 import StatusBadge from "@/components/status-badge";
 import TicketPriority from "@/components/ticket-priority";
@@ -35,7 +36,9 @@ export default function DataTable({ tickets }: Props) {
 					<TableBody>
 						{tickets?.map((ticket) => (
 							<TableRow key={ticket.id} data-href="/">
-								<TableCell>{ticket.title}</TableCell>
+								<TableCell>
+									<Link href={`/tickets/${ticket.id}`}>{ticket.title}</Link>
+								</TableCell>
 								<TableCell>
 									<div className="flex justify-center">
 										<StatusBadge status={ticket.status} />
