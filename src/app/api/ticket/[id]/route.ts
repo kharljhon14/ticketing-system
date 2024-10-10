efce7ab83aa002211a03f8027ccb33fe7d5356a0
Kharl <enriquezkharl14@gmail.com>
2024-10-10 09:24:54 +0800

    [Feat] Added update ticket route

---
+++ src/app/api/ticket/[id]/route.ts
@@ -0,0 +1,41 @@
+import { type NextRequest, NextResponse } from "next/server";
+
+import { ticketSchema } from "@/schemas/ticket";
+
+import prisma from "../../../../../prisma/db";
+
+interface Props {
+	params: { id: string };
+}
+
+export async function PATCH(request: NextRequest, { params }: Props) {
+	const body = await request.json();
+
+	// Validate the body object
+	const validation = ticketSchema.safeParse(body);
+
+	// Return 400 if bad request
+	if (!validation.success) {
+		return NextResponse.json(validation.error.format(), { status: 400 });
+	}
+
+	// Check if ticket exist on the database
+	const ticket = await prisma.ticket.findUnique({
+		where: { id: Number.parseInt(params.id) },
+	});
+
+	// Return 404 if ticket does not exist
+	if (!ticket) {
+		return NextResponse.json({ error: "Ticket Not Found" }, { status: 404 });
+	}
+
+	// Update the ticket with the new values
+	const updateTicket = await prisma.ticket.update({
+		where: { id: Number.parseInt(params.id) },
+		data: {
+			...body,
+		},
+	});
+
+	return NextResponse.json(updateTicket);
+}
