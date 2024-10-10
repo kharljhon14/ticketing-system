30eedc5800d07029760a2eefd3079a355d24bdc8
Kharl <enriquezkharl14@gmail.com>
2024-10-10 23:19:25 +0800

    [Feat] Added view ticket with nav buttons

--- src/features/tickets/ticket-details.tsx
+++ src/features/tickets/ticket-details.tsx
@@ -1,4 +1,17 @@
 import type { Ticket } from "@prisma/client";
+import Link from "next/link";
+
+import StatusBadge from "@/components/status-badge";
+import TicketPriority from "@/components/ticket-priority";
+import { buttonVariants } from "@/components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 
 interface Props {
 	ticket: Ticket;
@@ -6,9 +19,55 @@ interface Props {
 
 export default function TicketDetails({ ticket }: Props) {
 	return (
-		<div>
-			<p>{ticket.title}</p>
-			<p>{ticket.description}</p>
+		<div className="lg:grid lg:grid-cols-4">
+			<Card className="mx-4 mb-4 lg:col-span-3 lg:mr-4">
+				<CardHeader>
+					<div className="mb-3 flex justify-between">
+						<StatusBadge status={ticket.status} />
+						<div>
+							<TicketPriority priority={ticket.priority} />
+						</div>
+					</div>
+					<CardTitle>{ticket.title}</CardTitle>
+					<CardDescription>
+						Created:
+						{ticket.createdAt.toLocaleDateString("en-US", {
+							year: "2-digit",
+							month: "2-digit",
+							day: "2-digit",
+							hour: "numeric",
+							minute: "2-digit",
+							hour12: true,
+						})}
+					</CardDescription>
+				</CardHeader>
+				<CardContent>{ticket.description}</CardContent>
+				<CardFooter>
+					Updated:
+					{ticket.updatedAt.toLocaleDateString("en-US", {
+						year: "2-digit",
+						month: "2-digit",
+						day: "2-digit",
+						hour: "numeric",
+						minute: "2-digit",
+						hour12: true,
+					})}
+				</CardFooter>
+			</Card>
+			<div className="mx-4 flex gap-2 lg:mx-0 lg:flex-col">
+				<Link
+					href={`/tickets/edit/${ticket.id}`}
+					className={`${buttonVariants({ variant: "default" })}`}
+				>
+					Edit Ticket
+				</Link>
+				<Link
+					href={`/tickets/edit/${ticket.id}`}
+					className={`${buttonVariants({ variant: "default" })}`}
+				>
+					Delete Ticket
+				</Link>
+			</div>
 		</div>
 	);
 }
