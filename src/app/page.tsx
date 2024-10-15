f158b36322fa285ec6ed272eaf8e8f85549b5344
Kharl <enriquezkharl14@gmail.com>
2024-10-15 09:30:18 +0800

    [Feat] Added dashboard chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,13 @@ export default async function Home() {
     },
   });
 
+  const data = groupTicket.map((item) => {
+    return {
+      name: item.status,
+      total: item._count.id,
+    };
+  });
+
   return (
     <div>
       <div className="grid gap-4 md:grid-cols-2 px-2">
@@ -32,7 +39,7 @@ export default async function Home() {
           <RecentTickets tickets={tickets} />
         </div>
         <div>
-          <DashChart />
+          <DashChart data={data} />
         </div>
       </div>
     </div>
--- src/components/dash-chart.tsx
+++ src/components/dash-chart.tsx
@@ -1,7 +1,52 @@
-export default function DashChart() {
+'use client';
+
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
+
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
+
+interface DataElement {
+  name: string;
+  total: number;
+}
+
+interface Props {
+  data: DataElement[];
+}
+
+export default function DashChart({ data }: Props) {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Card className="col-span-4">
+      <CardHeader>
+        <CardTitle>Ticket Counts</CardTitle>
+      </CardHeader>
+      <CardContent className="pl-2">
+        <ResponsiveContainer
+          width="100%"
+          height={350}
+        >
+          <BarChart data={data}>
+            <XAxis
+              dataKey="name"
+              stroke="#888888"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+            />
+            <YAxis
+              stroke="#888888"
+              fontSize={12}
+              tickLine={false}
+              axisLine={false}
+            />
+            <Bar
+              dataKey="total"
+              fill="#60A5FA"
+              radius={[4, 4, 0, 0]}
+            />
+          </BarChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
   );
 }
+
